a5ee173638f29b13cbcc028a11f59f9445343587
CharlesKausihan <c.kausihan@hotmail.com>
2016-10-31 13:32:30 +0000

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,11 @@
+#reverse words in a sentence
+
+def reverseSentence(s):
+  x = s.split() #splits sentence
+  result = [] #blank results list 
+  for word in x: #loops through words
+    result.insert(0,word) #inserts words at start of results list
+  return " ".join(result) #joins words back together
+
+s = str(input('Enter sentence: '))
+print(reverseSentence(s))
