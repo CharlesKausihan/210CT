da93bf70be07f3d3da37ff6e5a784f1fa9f318ba
CharlesKausihan <c.kausihan@hotmail.com>
2016-11-01 15:44:31 +0000

    Update Q3 - Code.py

--- Q3 - Code.py
+++ Q3 - Code.py
@@ -1,15 +1,12 @@
 #Highest perfect square of a number
 
-def highestPerfectSquare(s):
-    x = 0
-    if s >= 0:
-        while x*x < s:
-            x +=1 #Keep adding 1 to x
-        if x*x != s:
-            x=x-1 #Take 1 away from x if x*x is bigger than s
-            print(x)
-        else: print(x)
-    else: print(str(s)+' is not a positive number')
-
-s = (int(input('Input number: ')))
-print(highestPerfectSquare(s))
+s = (int(input('Input number: ')))		    
+x = 0						
+if s >= 0:					
+    while x*x < s: 					
+        x +=1					
+    if x*x != s: 					
+        x=x-1					
+        print(x)		
+    else: print(x)
+else: print(str(s)+ 'is not a positive number')
