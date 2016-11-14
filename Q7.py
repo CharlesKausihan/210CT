a4e6ba6f55c84808e42f74d6cc400fb43e773e0c
CharlesKausihan <c.kausihan@hotmail.com>
2016-11-14 13:51:07 +0000

    Update Q7.py

--- Q7.py
+++ Q7.py
@@ -4,11 +4,12 @@ n = int(input("Enter a number: "))
 
 if n > 1: #prime numbers are greater than 1
     for i in range(2,n): #numbers from 2 to n-1
-       if (n % i) != 0: #check if dividing n by numbers from 2 to n-1 gives remainder
-           print(n,"is a prime number")
+       if (n % i) == 0: #check if dividing n by numbers from 2 to n-1 gives remainder
+           print(n,"is not a prime number")
            break
+
     else:
-        print(n,"is not a prime number")
+        print(n,"is a prime number")
        
 else:
     print(n,"is not a prime number")
