40607de1b5839e73ecbf787185944866f9c56d59
CharlesKausihan <c.kausihan@hotmail.com>
2016-11-28 22:32:44 +0000

    Update Q8.py

--- Q8.py
+++ Q8.py
@@ -1,13 +1,12 @@
-#Remove vowels from words
-
 def removeVowels(w):
     vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
-    if not w: #if not string
-        return w
-    elif w[0] in vowels: #checks first letter of word
-        return removeVowels(w[1:]) #checks remaining letter in word
-    else:
-        return w[0] + removeVowels(w[1:]) #checks whole word 
+    newString = ""
+    for i in w: 
+        if i not in vowels: #remove the non-vowels from string
+            newString = newString+i
+    w = newString
+    return w
 
 w = str(input('Enter word: '))
 print(removeVowels(w))
+
