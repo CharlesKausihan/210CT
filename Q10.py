3a58073f086bd7fd496a01819e30955fc93f14ae
CharlesKausihan <c.kausihan@hotmail.com>
2016-12-01 23:17:04 +0000

    Update Q10.py

--- Q10.py
+++ Q10.py
@@ -1,25 +1,25 @@
 #Longest increasing subsequence
 
 sequence = [1,2,3,4,1,5,1,6,7]
-longest_first = -1
+longestFirst = -1
 length = 0
-longest_length = 0
+longestLength = 0
 
-
+#Check if element in higher or lower than the last element
 for i in range(len(sequence)):
-    if (i + 1 == len(sequence)): #Check if its the end of the array
-        if (length > longest_length):
-            longest_length = length
-            longest_first = i - longest_length
+    if (i + 1 == len(sequence)): #Check if it is at the end of the array
+        if (length > longestLength): 
+            longestLength = length
+            longestFirst = i - longestLength
             break
         
-    elif (sequence[i] < sequence[i + 1]): #Check if index is smaller than the next index
+    elif (sequence[i] < sequence[i + 1]): #Check if elements is smaller than the next elements
         length += 1
 
-    elif (length >= longest_length): #Check if length is longer than longest to change the length
-        longest_length = length
-        longest_first = i - length #Longest is changed to first to see where to start from
+    elif (length >= longestLength): #Check if length is bigger than longestLength to change the length
+        longestLength = length
+        longestFirst = i - length #Longest is changed to first to see where to start from
         length = 0
 
-for i in range(longest_first, longest_first + longest_length+1):
+for i in range(longestFirst, longestFirst + longestLength+1):
     print(sequence[i])
