7b4d64d6f8b47cfc1dcaa780cd09aa63f38283b8
CharlesKausihan <c.kausihan@hotmail.com>
2016-12-01 23:23:47 +0000

    Update Q11.py

--- Q11.py
+++ Q11.py
@@ -1,56 +1,63 @@
-#Double linked list node delete function
+#TREE_SORT algorithm with INORDER function implemented iteratively.
 
-class Node(object):
-  def __init__(self, value):
-      self.value=value
-      self.next=None
-      self.prev=None
+class BinTreeNode(object):
+ 
+    def __init__(self, value):
+        self.value=value
+        self.left=None
+        self.right=None
 
-class List(object):
-
-  def __init__(self):
-      self.head=None
-      self.tail=None
-      
-  def insert(self,n,x):
-      if n!=None:
-          x.next=n.next
-          n.next=x
-          x.prev=n
-          
-          if x.next!=None:
-              x.next.prev=x
-              
-      if self.head==None:
-          self.head=self.tail=x
-          x.prev=x.next=None
-      elif self.tail==n:
-          self.tail=x
-
-
-  def remove(self, n):
-    if n.prev != None:
-          n.prev.next = n.next
+ 
+def tree_insert( tree, item):
+    if tree==None:
+        tree=BinTreeNode(item)
     else:
-        self.head = n.next
-    if n.next != None:
-        n.next.prev = n.prev
-    else:
-        self.tail = n.prev
+        if(item < tree.value):
+            if(tree.left==None):
+                tree.left=BinTreeNode(item)
+            else:
+                tree_insert(tree.left,item)
+        else:
+            if(tree.right==None):
+                tree.right=BinTreeNode(item)
+            else:
+                tree_insert(tree.right,item)
+    return tree
 
-          
-  def display(self):
-      values=[]
-      n=self.head
-      while n!=None:
-          values.append(str(n.value))
-          n=n.next
-      print ("List:",",".join(values))
-      
+ 
+def postorder(tree):
+    if(tree.left!=None):
+        postorder(tree.left)
+    if(tree.right!=None):
+        postorder(tree.right)
+    print (tree.value)
+ 
+ 
+def in_order(tree):
+    stackOrder = [] #Creating the stack
+    end = False
+    while(not end):
+        if tree != None:
+            stackOrder.append(tree) #Adds to tree
+            tree = tree.left #Moves to left of tree
+
+        else:
+            if(len(stackOrder) > 0):
+               tree = stackOrder.pop() #Pop the element
+               print(tree.value)
+               tree = tree.right #Moves to right of tree
+
+            else:
+                break
+
+ 
 if __name__ == '__main__':
-  l=List()
-  l.insert(None, Node(4))
-  l.insert(l.head,Node(6))
-  l.insert(l.head,Node(8))
-  l.remove(l.head)
-  l.display()
+   
+  t=tree_insert(None,6);
+  tree_insert(t,10)
+  tree_insert(t,5)
+  tree_insert(t,2)
+  tree_insert(t,3)
+  tree_insert(t,4)
+  tree_insert(t,11)
+  in_order(t)
